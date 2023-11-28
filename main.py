9fb80f5a9f2c0f069ab8831b25f81fb6b366d2de
bahodirmuhiddinov <bmuhiddinov@gmail.com>
2023-11-28 13:38:50 +0500

    missiya bajarildi

---
+++ main.py
@@ -0,0 +1,74 @@
+from funksiyalar import num
+from funksiyalar import sum
+from funksiyalar import name
+from nomer2f  import haqiqiybulish
+from nomer2f  import bulish
+from nomer2f  import foizlibulish
+from Bahodir  import dustm
+from Bahodir  import akm
+from Bahodir  import ukam
+from malumot  import main
+from malumot  import ism
+from malumot  import yosh
+from taxt     import revyasher
+from taxt     import ledink
+from taxt     import shut
+from uyilar   import gta
+from uyilar   import pubg
+from uyilar   import kontr
+
+a = num()
+print(a)
+
+b = sum()
+print(b)
+
+c = name()
+print(c)
+
+
+d = dustm()
+print(d)
+
+f = akm()
+print(f)
+
+g = ukam()
+print(g)
+
+h = main()
+print(h)
+
+j = ism()
+print(j)
+
+k = yosh()
+print(k)
+
+l = haqiqiybulish()
+print(l)
+
+z = bulish()
+print(z)
+
+x = foizlibulish()
+print(x)
+
+v = revyasher()
+print(v)
+
+n = ledink()
+print(n)
+
+
+m = shut()
+print(m)
+
+de = gta()
+print(de)
+
+ha = pubg()
+print(ha)
+
+yoq = kontr()
+print(yoq)
